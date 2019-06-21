e226726de3781d6a1c094c32fdf8ceff3b972cba
dannythompson901 <dannythompson901@gmail.com>
2019-06-21 06:18:09 -0500

    first version of the unfinished car booking. sql database built.

---
+++ src/test/java/com/dannythompsondev/bookings/BookingsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.dannythompsondev.bookings;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BookingsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
